fcf70543915d6515b10d740850ca00a78d3a687d
RichardBruskiewich <richard.bruskiewich@delphinai.com>
2018-12-19 06:04:26 +0000

    Clean up staging project restructuring

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 echo "Deploying Divseek Canada Portal"
 
+echo "Baking a new docker-compose with environmental variables from .env"
 source .env
 envsubst < "docker-compose-template.yml" > "docker-compose.yml"
+echo "Created docker-compose.yml"
 
+echo "Cycling docker-compose (down->build->up)"
 docker-compose down
 docker-compose build
-docker-compose up
+docker-compose -f docker-compose.yml up -d
+
+echo "Use docker-compose logs to see deployment progress"
