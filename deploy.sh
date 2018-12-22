b5d77728b5cb3b574a0cf3398664b438c09c7724
Richard Bruskiewich <richard.bruskiewich@delphinai.com>
2018-12-22 07:24:40 +0000

    reduce Tripal startup wait in deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ docker-compose up -d apollo_db tripal_db
 sleep 30s
 echo "Staring up Tripal"
 docker-compose up -d tripal
-sleep 5m
+sleep 3m
 echo "Starting up the remainder of the system including the proxy"
 docker-compose up -d
 
